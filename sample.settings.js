41b3b7ad6d315f2a69bd0ceac2ee067cf8deb5dc
Raghu Saxena <poiasdpoiasd@live.com>
2020-10-09 06:48:09 +0200

    Discord disabled by default

--- sample.settings.js
+++ sample.settings.js
@@ -5,7 +5,7 @@ exports.SETTINGS = {
     CONCURRENT_RACES: 1, //If these many races (downloads in qbit) are going on, then the incoming torrent will be skipped
     COUNT_STALLED_DOWNLOADS: false, //These are cases when the seeder abandons. Sets whether to count them while checking CONCURRENT_RACES. Afvisable to set to false, because if CONCURRENT_RACES is 1, and a seeder abandons, this will skip all future races even though box is idle
     DISCORD_NOTIFICATIONS: {
-        enabled: true,
+        enabled: false,
         botUsername: 'qBittorrent',
         botAvatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/New_qBittorrent_Logo.svg/600px-New_qBittorrent_Logo.svg.png'
     }
