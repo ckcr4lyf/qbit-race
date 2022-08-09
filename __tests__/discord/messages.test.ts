991b67ce90901b88dda3c22b06cb3dd205271236
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-09 21:29:51 +0800

    test for torrent added body

--- __tests__/discord/messages.test.ts
+++ __tests__/discord/messages.test.ts
@@ -1,18 +1,18 @@
 import crypto from 'node:crypto';
 import test from 'ava';
 
-import { buildMessageBody } from '../../src/discord/messages.js';
+import { buildMessageBody, buildTorrentAddedBody } from '../../src/discord/messages.js';
+
+const fakeDiscSettings = {
+    enabled: true,
+    botUsername: 'bot',
+    botAvatar: 'image.png'
+}
 
 test('buildMessageBody', t => {
     const aVal = crypto.randomBytes(10).toString('hex');
     const bVal = crypto.randomBytes(10).toString('hex');
     const partialData: any = { a: aVal, b: bVal };
-
-    const fakeDiscSettings = {
-        enabled: true,
-        botUsername: 'bot',
-        botAvatar: 'image.png'
-    }
 
     const want = {
         a: aVal,
@@ -22,6 +22,48 @@ test('buildMessageBody', t => {
     };
 
     const got = buildMessageBody(fakeDiscSettings, partialData);
+
+    t.deepEqual(got, want);
+})
+
+test('buildTorrentAddedBody', t => {
+    const torrentInfo: any = {
+        size: 2.5 * 1024 * 1024, // 2.5MiB
+        name: 'arch',
+        trackers: ['archlinux.org'],
+        reannounceCount: 3,
+    };
+
+    const want = {
+        username: 'bot',
+        avatar_url: 'image.png',
+        content: `Added arch (2.50 MiB)`,
+        embeds: [
+            {
+                title: 'arch',
+                description: 'Added to qBittorrent',
+                thumbnail: {
+                    url: 'image.png',
+                },
+                fields: [
+                    {
+                        name: 'Tracker',
+                        value: 'archlinux.org',
+                    },
+                    {
+                        name: 'Size',
+                        value: '2.50 MiB'
+                    },
+                    {
+                        name: 'Reannounce Count',
+                        value: '3'
+                    }
+                ]
+            }
+        ]
+    }
+
+    const got = buildTorrentAddedBody(fakeDiscSettings, torrentInfo);
 
     t.deepEqual(got, want);
 })
