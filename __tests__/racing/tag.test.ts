b926d59c9293670a7e5a2e2faba0ac2f999f2a8b
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-20 12:21:20 +0800

    Dont tag in dry run

--- __tests__/racing/tag.test.ts
+++ __tests__/racing/tag.test.ts
@@ -66,3 +66,31 @@ test('tagWhenNotWorking', async t => {
     t.deepEqual(third.called, true);
     t.deepEqual(third.calledWith([{ hash: 'GGWP' }] as any, ['error']), true);
 })
+
+test('dontTagIfDryRun', async t => {
+    const mockApi = newMockQbitApi();
+    // Test with torrents
+    const first = sinon.stub(mockApi, 'getTorrents').resolves([
+        {
+            hash: 'ABCD',
+        },
+        {
+            hash: 'GGWP'
+        }
+    ] as any);
+
+    const second = sinon.stub(mockApi, 'getTrackers').onCall(0).resolves(getMockWorkingTrackers()).onCall(1).resolves(getMockNotWorkingTrackers())
+
+    const third = sinon.stub(mockApi, 'addTags').onCall(0).resolves(undefined);
+
+    // Shouldnt tag in dry run
+    const result = await tagErroredTorrents(mockApi, true);
+    t.deepEqual(result, undefined);
+    t.deepEqual(first.called, true);
+    t.deepEqual(second.called, true);
+    t.deepEqual(second.calledWith('ABCD'), true);
+    t.deepEqual(second.calledWith('GGWP'), true);
+
+    // Since second has a non working tracker, we expect a tag as well
+    t.deepEqual(third.called, false);
+})
