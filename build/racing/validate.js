5a49399a3f6af091ba33c6c8b01a38d7de113d81
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-20 12:23:39 +0800

    Get rid of validate

--- build/racing/validate.js
+++
@@ -1,5 +0,0 @@
-/**
- * Validate will attempt to connect to qBittorrent using the supplied
- * config. If webhooks are enabled, it will also send a test webhook.
- */
-//# sourceMappingURL=validate.js.map
