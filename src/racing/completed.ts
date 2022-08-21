1e4e8236962f02aec7725a2876299613587b9063
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-21 21:34:38 +0800

    update handler

--- src/racing/completed.ts
+++ src/racing/completed.ts
@@ -1,4 +1,4 @@
-import { QbittorrentApi } from "../qbittorrent/api.js";
+import { QbittorrentApi, QbittorrentTorrent } from "../qbittorrent/api.js";
 import { Settings } from "../utils/config";
 import { getLoggerV3 } from "../utils/logger.js";
 
@@ -8,15 +8,26 @@ export const postRaceResumeV2 = async (api: QbittorrentApi, settings: Settings, 
 
     const torrentInfo = await api.getTorrent(infohash);
 
+    // Handle category change stuff if applicable
     if (torrentInfo.category in settings.CATEGORY_FINISH_CHANGE){
-
         const newCategory = settings.CATEGORY_FINISH_CHANGE[torrentInfo.category];
         logger.debug(`Found entry in category change map. Changing from ${torrentInfo.category} to ${newCategory}`);
-        
 
+        try {
+            await api.setCategory(torrentInfo.hash, newCategory);
+            logger.debug(`Successfully set category to ${newCategory}`);
+        } catch (e){
+            logger.error(`Failed to set category. ${e}`);
+        }
     }
 
+    let torrents: QbittorrentTorrent[];
 
-
+    try {
+        torrents = await api.getTorrents();
+    } catch (e){
+        logger.error(`Failed to get torrents list from API! ${e}`);
+        process.exit(1);
+    }
 
 }
