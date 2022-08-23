2db93c6d735c1891c6d82d6dc064162605ad755c
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-23 19:24:59 +0800

    Logic for torrents still in progress

--- src/racing/completed.ts
+++ src/racing/completed.ts
@@ -1,12 +1,22 @@
+import { sendMessageV2 } from "../discord/api.js";
+import { buildTorrentCompletedBody } from "../discord/messages.js";
+import { TorrentState } from "../interfaces.js";
 import { QbittorrentApi, QbittorrentTorrent } from "../qbittorrent/api.js";
 import { Settings } from "../utils/config";
 import { getLoggerV3 } from "../utils/logger.js";
 
 export const postRaceResumeV2 = async (api: QbittorrentApi, settings: Settings, infohash: string) => {
-
     const logger = getLoggerV3();
+    let torrentInfo: QbittorrentTorrent;
 
-    const torrentInfo = await api.getTorrent(infohash);
+    try {
+        torrentInfo = await api.getTorrent(infohash);
+    } catch (e){
+        logger.error(`Failed to get torrent from API ${e}`)
+        throw e;
+    }
+
+    logger.info(`${torrentInfo.name} completed... running post race script`);
 
     // Handle category change stuff if applicable
     if (torrentInfo.category in settings.CATEGORY_FINISH_CHANGE){
@@ -32,9 +42,44 @@ export const postRaceResumeV2 = async (api: QbittorrentApi, settings: Settings, 
 
     // Handle discord part if enabled
     if (settings.DISCORD_NOTIFICATIONS.enabled === true){
+        const messageBody = buildTorrentCompletedBody(settings.DISCORD_NOTIFICATIONS, torrentInfo);
 
-        
-
+        try {
+            await sendMessageV2(settings.DISCORD_NOTIFICATIONS.webhook, messageBody);
+            logger.debug(`Sent message to discord`);
+        } catch (e){
+            logger.error(`Failed to send message to discord ${e}`);
+        }
     }
 
+    //TODO: Need to resume torrents
+
+    // The earliest time, from which there may still be a torrent in the reannounce phase
+    // e.g. if interval is 10s, and limit is 6, then from now-60s, any torrents in stalledDL status
+    // are still in the re-announce phase (for racing) , so we do not run the resume job
+    const oldestRaceLimit = Date.now() - (settings.REANNOUNCE_INTERVAL * settings.REANNOUNCE_LIMIT);
+
+    for(let x = 0; x < torrents.length; x++){
+        if (torrents[x].state === TorrentState.downloading){
+            logger.debug(`${torrents[x].name} is still downloading, won't resume`);
+            return;
+        }
+
+        if (torrents[x].state === TorrentState.stalledDL && torrents[x].added_on * 1000 > oldestRaceLimit){
+            logger.debug(`${torrents[x].name} is in re-announce phase, won't resume`);
+            return;
+        }
+    }
+
+    // Check if anything is paused
+    const pausedTorrents = torrents.filter(torrent => torrent.state === TorrentState.pausedUP);
+
+    if (pausedTorrents.length === 0){
+        logger.debug(`Nothing to resume (no paused torrents)`);
+        return;
+    }
+
+    // Try and resume
+    // TODO: Add into API!
+    // api.resumeTorrents(pausedTorrents);
 }
