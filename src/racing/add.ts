1f04d655efd823712c98422210ff8362313821d8
Raghu Saxena <poiasdpoiasd@live.com>
2022-08-29 20:28:59 +0800

    Upto pre race check

--- src/racing/add.ts
+++ src/racing/add.ts
@@ -2,10 +2,37 @@
  * To add a new torrent into qbittorrent for racing
  */
 
-import { getLoggerV3 } from "../utils/logger"
+import { getTorrentMetainfo, torrentMetainfo } from "../helpers/torrent.js";
+import { getLoggerV3 } from "../utils/logger.js"
+import * as fs from 'fs';
+import { Settings } from "../utils/config";
+import { QbittorrentApi } from "../qbittorrent/api";
 
-export const addTorrentToRace = (path: string, category?: string) => {
+export const addTorrentToRace = (api: QbittorrentApi, settings: Settings, path: string, category?: string) => {
 
     const logger = getLoggerV3();
     logger.debug(`Called with path: ${path}, category: ${category}`);
+
+    // Read the torrent file and get info
+    let torrentFile: Buffer;
+    
+
+    try {
+        torrentFile = fs.readFileSync(path);
+    } catch (e){
+        logger.error(`Failed to read torrent from ${e}`);
+        process.exit(1);
+    }
+
+    let torrentMetainfo: torrentMetainfo;
+
+    try {
+        torrentMetainfo = getTorrentMetainfo(torrentFile);
+    } catch (e){
+        logger.error(`Fail to parse torrent file`);
+        process.exit(1);
+    }
+
+    // Do pre race check to determine if we should add this torrent    
+
 }
