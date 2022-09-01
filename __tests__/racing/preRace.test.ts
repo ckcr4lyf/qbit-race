23a075119fa1ea3de2854e83989559f966e2cba0
Raghu Saxena <poiasdpoiasd@live.com>
2022-09-02 00:07:36 +0800

    concurrent race limit hit

--- __tests__/racing/preRace.test.ts
+++ __tests__/racing/preRace.test.ts
@@ -3,9 +3,30 @@ import test from 'ava';
 
 import { concurrentRacesCheck } from '../../src/racing/preRace.js';
 import { defaultSettings } from '../../src/utils/config.js';
+import { QbittorrentTorrent } from '../../src/qbittorrent/api.js';
+import { TorrentState } from '../../src/interfaces.js';
 
 test('concurrentRacesNoTorrents', t => {
     const settings = { ...defaultSettings };
     const result = concurrentRacesCheck(settings, [])
     t.deepEqual(result, true);
 });
+
+test('concurrentRacesAndDownloading', t => {
+    const settings = { ...defaultSettings };
+    settings.CONCURRENT_RACES = 2;
+    const torrents: any[] = [
+        {
+            state: TorrentState.downloading
+        },
+        {
+            state: TorrentState.downloading
+        },
+        {
+            state: TorrentState.uploading
+        }
+    ];
+
+    const result = concurrentRacesCheck(settings, torrents);
+    t.deepEqual(result, false);
+})
