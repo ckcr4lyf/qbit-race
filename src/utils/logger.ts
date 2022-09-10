88f80d8755e3bd774a9a35b20308c5de81cad5cc
Raghu Saxena <poiasdpoiasd@live.com>
2022-09-10 13:43:30 +0800

    New logger, options

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,5 +3,7 @@ import { Logger, LOGLEVEL } from "@ckcr4lyf/logger";
 export const getLoggerV3 = (): Logger => {
     // Hardcoded to DEBUG.
     // In future get from user's settings or env var
-    return new Logger(LOGLEVEL.DEBUG);
+    return new Logger({
+        loglevel: LOGLEVEL.DEBUG,
+    });
 }
