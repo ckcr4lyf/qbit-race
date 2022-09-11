5cc37b3d04824265cc4765041872f43fa177c358
Raghu Saxena <poiasdpoiasd@live.com>
2022-09-11 11:26:06 +0800

    build as well:

--- build/src/server/app.js
+++ build/src/server/app.js
@@ -2,18 +2,30 @@
 import fastify from 'fastify';
 import { getLogger } from '../helpers/logger.js';
 import { makeMetrics, stateMetrics } from '../helpers/preparePromMetrics.js';
-import { getTorrents, getTransferInfo } from '../qbittorrent/api.js';
+import { getTransferInfo } from '../qbittorrent/api.js';
+import { loadConfig, makeConfigIfNotExist } from '../utils/configV2.js';
 const server = fastify();
 const logger = getLogger(`prom-exporter`);
+makeConfigIfNotExist();
+const config = loadConfig();
+let api;
+try {
+    // api = await loginV2(config.QBITTORRENT_SETTINGS);
+}
+catch (e) {
+    logger.error(`Failed to login: ${e}`);
+    process.exit;
+}
 // For this application, set the API log to prom-exporter
 server.get('/metrics', async (request, reply) => {
     // TOOD: LoginV2 with api returned...
     // await login();
     const transferInfo = await getTransferInfo();
-    const torrents = await getTorrents();
+    // const torrents = await getTorrents();
     let finalMetrics = '';
     finalMetrics += makeMetrics(transferInfo);
-    finalMetrics += stateMetrics(torrents);
+    // finalMetrics += stateMetrics(torrents);
+    finalMetrics += stateMetrics([]);
     reply.status(200).headers({
         'Content-Type': 'text/plain'
     }).send(finalMetrics);
